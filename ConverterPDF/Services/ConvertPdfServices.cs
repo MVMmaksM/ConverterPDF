3ad63dd9b3e93d9e92abcb44dd1b765d9d54724f
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-08-25 13:02:10 +0500

    Create ConvertPdfServices

---
+++ ConverterPDF/Services/ConvertPdfServices.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Office.Interop.Word;
+using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ConverterPDF.Services
+{
+    public class ConvertPdfServices : IConvertPdfServices
+    {
+        public void ConvertExcelToPdf(List<string> pathExcelFiles)
+        {
+            var appExcel = new Excel.Application();
+            appExcel.Visible = true;
+            Excel.Workbook workbook = null;
+            var pathFileConverting = string.Empty;
+
+            try
+            {
+                foreach (var pathFile in pathExcelFiles)
+                {
+                    pathFileConverting = pathFile;
+
+                    workbook = appExcel.Workbooks.Open(pathFile); //к вашей книге
+                    appExcel.ActiveWorkbook.ExportAsFixedFormat(Excel.XlFixedFormatType.xlTypePDF, Path.ChangeExtension(pathFile, ".pdf"));//куда сохраняете
+                    workbook.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ошибка при конвертации файла: {pathFileConverting}", ex);
+            }
+            finally 
+            {
+                workbook.Close();
+                appExcel.Quit();
+            }            
+        }
+
+        public void ConvertPowerPointToPdf(List<string> pathPowerPointFiles)
+        {
+            //var appPowerPoint = new PowerPoint.Application();
+            //PowerPoint.Presentation docPres = appPowerPoint.Presentations.Open(@"C:\\Users\\p45_VaganovMV\\Desktop\\Result.pdf");
+            //appPowerPoint.ActivePresentation.ExportAsFixedFormat(pathPdf, PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF);
+            //docPres.Close();
+            //appPowerPoint.Quit();
+        }
+        public void ConvertWordToPdf(List<string> pathWordFiles)
+        {
+           var appWord = new Word.Application();
+            appWord.Visible = true;
+            Word.Document document = null;
+            var pathFileConverting = string.Empty;
+
+            foreach (var pathFile in pathWordFiles)
+            {
+                pathFileConverting = pathFile;
+
+                document = appWord.Documents.Open(pathFile);
+                appWord.ActiveDocument.ExportAsFixedFormat(Path.ChangeExtension(pathFile, ".pdf"), Word.WdExportFormat.wdExportFormatPDF);
+                document.Close();
+            }
+             
+            appWord.Quit();
+        }
+    }
+}
---
+++ ConverterPDF/Services/IConvertPdfServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterPDF.Services
+{
+    public interface IConvertPdfServices
+    {
+        void ConvertWordToPdf(List<string> pathWordFiles);
+        void ConvertExcelToPdf(List<string> pathExcelFiles);
+        void ConvertPowerPointToPdf(List<string> pathPowerPointFiles);
+    }
+}
